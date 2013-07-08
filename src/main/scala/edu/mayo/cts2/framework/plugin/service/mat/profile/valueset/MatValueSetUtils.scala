afa53b1bff1fc7111e65f1e484a16845f58f4194
suesse <dale.suesse@live.com>
2013-07-08 08:57:46 -0500

    Removes unused imports

--- src/main/scala/edu/mayo/cts2/framework/plugin/service/mat/profile/valueset/MatValueSetUtils.scala
+++ src/main/scala/edu/mayo/cts2/framework/plugin/service/mat/profile/valueset/MatValueSetUtils.scala
@@ -1,32 +1,8 @@
 package edu.mayo.cts2.framework.plugin.service.mat.profile.valueset
 
-import scala.collection.JavaConversions.iterableAsScalaIterable
-import org.springframework.context.annotation.Scope
-import org.springframework.stereotype.Component
-import org.springframework.stereotype.Repository
-import org.springframework.transaction.annotation.Transactional
-import edu.mayo.cts2.framework.model.command.Page
-import edu.mayo.cts2.framework.model.core.PredicateReference
-import edu.mayo.cts2.framework.model.core.SortCriteria
-import edu.mayo.cts2.framework.model.directory.DirectoryResult
-import edu.mayo.cts2.framework.model.valueset.ValueSetCatalogEntry
-import edu.mayo.cts2.framework.model.valueset.ValueSetCatalogEntrySummary
 import edu.mayo.cts2.framework.plugin.service.mat.model.ValueSet
-import edu.mayo.cts2.framework.plugin.service.mat.profile.AbstractService
 import edu.mayo.cts2.framework.plugin.service.mat.repository.ValueSetRepository
-import edu.mayo.cts2.framework.service.profile.valueset.ValueSetQuery
-import edu.mayo.cts2.framework.service.profile.valueset.ValueSetQueryService
-import javax.annotation.Resource
-import javax.persistence.Entity
-import edu.mayo.cts2.framework.model.core.MatchAlgorithmReference
-import edu.mayo.cts2.framework.model.core.PropertyReference
-import org.springframework.context.annotation.ScopedProxyMode
 import scala.collection.JavaConversions._
-import edu.mayo.cts2.framework.service.meta.StandardMatchAlgorithmReference
-import edu.mayo.cts2.framework.service.meta.StandardModelAttributeReference
-import org.springframework.data.domain.Pageable
-import org.apache.commons.collections.CollectionUtils
-import edu.mayo.cts2.framework.model.command.ResolvedFilter
 import edu.mayo.cts2.framework.model.core.ValueSetDefinitionReference
 import edu.mayo.cts2.framework.model.core.ValueSetReference
 import edu.mayo.cts2.framework.model.core.NameAndMeaningReference
@@ -34,10 +10,8 @@ import edu.mayo.cts2.framework.core.url.UrlConstructor
 import edu.mayo.cts2.framework.model.core.SourceReference
 import edu.mayo.cts2.framework.model.core.SourceAndRoleReference
 import edu.mayo.cts2.framework.model.core.RoleReference
-import edu.mayo.cts2.framework.plugin.service.mat.uri.UriUtils
 import edu.mayo.cts2.framework.plugin.service.mat.model.ValueSetVersion
 import org.apache.commons.lang.StringUtils
-import edu.mayo.cts2.framework.plugin.service.mat.repository.ValueSetVersionRepository
 
 object MatValueSetUtils {
 
@@ -70,7 +44,7 @@ object MatValueSetUtils {
     urlConstructor: UrlConstructor): ValueSetDefinitionReference = {
 
     val valueSetDefName = getValueSetDefName(valueSetVersion)
- 
+
     buildValueSetDefinitionReference(
       name, about,
       valueSetDefName, valueSetVersion.documentUri,
